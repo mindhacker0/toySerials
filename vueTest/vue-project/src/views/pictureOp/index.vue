6bdbd188d70f52d5f8072cc4af6470e8bbb17d64
zhoupf <983213779@qq.com>
2024-04-15 03:29:14 +0800

    chore:move ffmpeg

--- vueTest/vue-project/src/views/pictureOp/index.vue
+++ vueTest/vue-project/src/views/pictureOp/index.vue
@@ -1,6 +1,6 @@
 <script setup>
 import { usePicture } from './usePicture';
-const {screenRef,selectFile,saveWorkFile} = usePicture();
+const {screenRef,selectFile,saveWorkFile,rotate90} = usePicture();
 </script>
 <template>
     <div>
@@ -10,6 +10,7 @@ const {screenRef,selectFile,saveWorkFile} = usePicture();
         <div>
             <button @click="selectFile">文件选择</button>
             <button @click="saveWorkFile">保存</button>
+            <button @click="rotate90">旋转</button>
         </div>
     </div>
 </template>
