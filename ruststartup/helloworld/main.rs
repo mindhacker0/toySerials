feec1bbb76d501b3535f85b04a8d3c494d564b3c
zhoupf <983213779@qq.com>
2024-11-14 04:18:58 +0800

    merge home

--- ruststartup/helloworld/main.rs
+++ ruststartup/helloworld/main.rs
@@ -1,3 +1,11 @@
+const TIME_IN_DAY:u32 = 1000*60*60*24;
 fn main(){
     println!("hello world!");
+    let mut x = 6;
+    let y = "    ";
+    println!("x is {}",x);
+    x = 7;
+    println!("x is {}",x);
+    println!("constant is {}",TIME_IN_DAY);
+    println!("len is {}",y.len());
 }
